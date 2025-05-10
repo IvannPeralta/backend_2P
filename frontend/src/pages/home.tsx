551487ffcb9024829599a4f8b4802405d4a6bf2f
Cesar Alonso <cesaralonso99@gmail.com>
2025-05-10 12:45:26 -0300

    feat: frontend setup

---
+++ frontend/src/pages/home.tsx
@@ -0,0 +1,15 @@
+
+//import api from '../api/axios';
+
+import { Button } from "@/components/ui/button";
+
+function HomePage() {
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default HomePage;
